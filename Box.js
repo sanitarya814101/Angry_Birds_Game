ba76945908990647dfe662d4e00819c9cba48a9e
sanitarya814101 <84065869+sanitarya814101@users.noreply.github.com>
2021-05-12 12:45:41 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,13 @@
+class Box extends BaseClass{
+
+    constructor(x,y,w,h){
+
+        super(x,y,w,h);
+        this.image = loadImage("sprites/Wood1.png");
+    }
+
+    display(){
+
+        super.display();
+    }
+}

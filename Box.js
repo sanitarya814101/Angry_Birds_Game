eea350def5290c98c8b9450931ef6147cd9cfd87
sanitarya814101 <84065869+sanitarya814101@users.noreply.github.com>
2021-07-15 08:56:59 +0530

    Update Box.js

--- Box.js
+++ Box.js
@@ -3,7 +3,7 @@ class Box extends BaseClass{
     constructor(x,y,w,h){
 
         super(x,y,w,h);
-        this.image = loadImage("sprites/wood2.png");
+        this.image = loadImage("sprites/wood1.png");
     }
 
     display(){
